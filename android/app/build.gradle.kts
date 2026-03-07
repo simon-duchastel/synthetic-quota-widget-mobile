93e5f9f8ba71ea60049524ffde89a231edbd0175
Simon Duchastel <simon@duchastel.com>
2026-03-07 16:24:33 -0500

    Add main activity (later will be more of a settings activity)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
