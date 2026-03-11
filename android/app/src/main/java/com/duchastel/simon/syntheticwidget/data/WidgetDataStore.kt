1a90acc2eaad1f2695f3e73166d13d54708af4e7
Simon Duchastel <simon@duchastel.com>
2026-03-10 22:03:18 -0700

    update imports

--- android/app/src/main/java/com/duchastel/simon/syntheticwidget/data/WidgetDataStore.kt
+++ android/app/src/main/java/com/duchastel/simon/syntheticwidget/data/WidgetDataStore.kt
@@ -1,21 +1,10 @@
 package com.duchastel.simon.syntheticwidget.data
 
 import android.content.Context
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.glance.appwidget.state.getAppWidgetState
-import androidx.glance.appwidget.state.updateAppWidgetState
-import com.duchastel.simon.syntheticwidget.widget.QuotaWidget
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class WidgetRepository @Inject constructor(
