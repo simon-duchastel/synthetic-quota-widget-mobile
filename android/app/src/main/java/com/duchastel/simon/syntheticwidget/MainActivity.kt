2d4272276537fbe253cd35ac3605a56882716ec1
Simon Duchastel <simon@duchastel.com>
2026-03-17 23:12:36 -0400

    Proper window insets

--- android/app/src/main/java/com/duchastel/simon/syntheticwidget/MainActivity.kt
+++ android/app/src/main/java/com/duchastel/simon/syntheticwidget/MainActivity.kt
@@ -12,12 +12,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -86,6 +90,7 @@ fun SyntheticWidgetApp(
     navController: NavHostController = rememberNavController()
 ) {
     NavHost(
+        modifier = Modifier.windowInsetsPadding(WindowInsets.displayCutout),
         navController = navController,
         startDestination = Screen.WidgetList.route
     ) {
